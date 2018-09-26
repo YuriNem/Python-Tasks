5ee45287e639c1f0014360e543fe6890a2ed1236
YuriNem <PsychoactivePie@gmail.com>
2018-09-26 17:30:39 +0300

    Updated icosahedron

--- icosahedron.py
+++ icosahedron.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
 edge = float(input('Input icosahedron edge length: '))
+print('\n')
 
 if edge <= 0:
     print('Uncorrect length')
@@ -10,7 +11,6 @@ else:
     radiusInscribedSphere = edge * sqrt(3) * (3 + sqrt(5)) / 12
     radiusDescribedSphere = edge * sqrt(2 * (5 + sqrt(5))) / 4
 
-    print('\n')
     print('Volume of icosahedron: {:.5g}'.format(volume))
     print('Surface area of icosahedron: {:.5g}'.format(surfaceArea))
     print('Radius inscribed sphere of icosahedron: {:.5g}'.format(radiusInscribedSphere))
